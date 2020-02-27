beadfdf3fd777508beff96a78f53ae8a4512a1ca
modesnero <modesnero@gmail.com>
2020-02-28 02:16:15 +0300

    add done func

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/app'
+import './../node_modules/font-awesome/css/font-awesome.min.css'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
