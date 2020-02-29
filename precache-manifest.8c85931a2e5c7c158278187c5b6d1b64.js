f2fa1844757e230375e68c85941ddc3b5a285515
modesnero <modesnero@gmail.com>
2020-02-29 12:18:49 +0300

    Updates

---
+++ precache-manifest.8c85931a2e5c7c158278187c5b6d1b64.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452eee5335abd26d540b793c254d092e",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "bf1abb2bee228a0c9e2d",
+    "url": "/todo-react/static/css/2.de3a215f.chunk.css"
+  },
+  {
+    "revision": "06cc777ef01d66e314ba",
+    "url": "/todo-react/static/css/main.0e6bbf39.chunk.css"
+  },
+  {
+    "revision": "bf1abb2bee228a0c9e2d",
+    "url": "/todo-react/static/js/2.dc57647c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-react/static/js/2.dc57647c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06cc777ef01d66e314ba",
+    "url": "/todo-react/static/js/main.6fe6246c.chunk.js"
+  },
+  {
+    "revision": "2428ea3caf2283ea2d88",
+    "url": "/todo-react/static/js/runtime-main.a22a98c4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
