8b9aff44e79d417f33e28319cd8058a6a8315efa
modesnero <modesnero@gmail.com>
2020-03-21 17:04:32 +0300

    Updates

---
+++ precache-manifest.81dbcf1cb4c834b6c7747021104bb53b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7698ae9127dbec9dd46042ec1e73ff80",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "a552fd3aac8d8a5aaba3",
+    "url": "/todo-app/static/css/2.b4fedc27.chunk.css"
+  },
+  {
+    "revision": "91cbc9a0af8190c27753",
+    "url": "/todo-app/static/css/main.3ab39e4f.chunk.css"
+  },
+  {
+    "revision": "a552fd3aac8d8a5aaba3",
+    "url": "/todo-app/static/js/2.dc652ca7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-app/static/js/2.dc652ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91cbc9a0af8190c27753",
+    "url": "/todo-app/static/js/main.fbb2d347.chunk.js"
+  },
+  {
+    "revision": "c4d46ae89b04c6bd74a0",
+    "url": "/todo-app/static/js/runtime-main.fe48852d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todo-react/precache-manifest.88849eddd92752781025de53efa3a720.js"
+  "/todo-app/precache-manifest.81dbcf1cb4c834b6c7747021104bb53b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
